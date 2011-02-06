53f92cf240102e2869d65bbce0c93279a140a0b2
Jack Christensen <jack@jackchristensen.com>
2011-02-06 14:40:37 -0600

    Using thor instead of rake.

--- lib/db_schemer.rb
+++ lib/db_schemer.rb
@@ -1,3 +1,5 @@
+require 'sequel'
+Sequel.extension :migration
 require 'yaml'
 
 class DbSchemer
--- lib/db_schemer/generators/new.rb
+++ lib/db_schemer/generators/new.rb
@@ -11,4 +11,18 @@ class DbSchemerCommand < Thor
     self.destination_root = path
     directory "new", "."
   end
+
+  desc "migrate", "Drops definitions, run alterations, then recreate definitions"
+  method_option :environment, :type => :string, :desc => "Database environment to load", :default => "development", :aliases => "-e"
+  method_option :alteration_version, :type => :numeric, :desc => "Target alteration version", :aliases => "-a"
+  method_option :definition_sequences, :type => :array, :desc => "Definition sequences to drop and create", :default => ["default"], :aliases => "-d"
+  def migrate
+    require 'yaml'
+
+    config = YAML.load(File.read('config.yml'))
+    db = Sequel.connect config['environments'][options["environment"]]
+    db_schemer = DbSchemer.new(db, config['alterations']['table'], config['alterations']['column'], config['definitions']['table'])
+
+    db_schemer.migrate(:version => options["alteration_version"], :sequences => options["definition_sequences"])
+  end
 end
